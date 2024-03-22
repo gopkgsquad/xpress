858c590bdc92c8f8a8298e84b90b19448bb263b3
ankesh <ankeshnirala@gmail.com>
2024-03-22 13:43:06 +0530

    logger and loader updated to latest

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/gopkgsquad/xpress
 go 1.22
 
 require (
-	github.com/gopkgsquad/gloader v1.0.0
-	github.com/gopkgsquad/glogger v1.0.1
+	github.com/gopkgsquad/gloader v1.0.1
+	github.com/gopkgsquad/glogger v1.0.2
 )
--- mux.router.go
+++ mux.router.go
@@ -1,13 +1,26 @@
 package xpress
 
 import (
+	"fmt"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 
 	"github.com/gopkgsquad/gloader"
 	"github.com/gopkgsquad/glogger"
+)
+
+// ANSI color escape codes
+const (
+	ColorRed     = "\033[31m"
+	ColorGreen   = "\033[32m"
+	ColorYellow  = "\033[33m"
+	ColorBlue    = "\033[34m"
+	ColorReset   = "\033[0m"
+	ColorMagenta = "\033[35m"
+	ColorCyan    = "\033[36m"
 )
 
 var _ Router = &MuxRouter{}
@@ -76,9 +89,41 @@ func (r *MuxRouter) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 
 	duration := time.Since(start)
 
-	r.logger.Infof("%v | %v | %s | %s | %s", capturer.status, formatResponseTime(duration), strings.SplitN(req.RemoteAddr, ":", 2)[0], req.Method, req.URL.Path)
+	statusColor := getColorForStatus(capturer.status)
+	methodColor := ColorCyan
+	pathColor := ColorGreen
+	timeColor := ColorMagenta
+	remoteAddrColor := ColorYellow
+
+	// Log request details with colors
+	r.logger.Infof("%s | %s | %s | %s | %s",
+		colorize(statusColor, strconv.Itoa(capturer.status)),
+		colorize(methodColor, req.Method),
+		colorize(pathColor, req.URL.Path),
+		colorize(timeColor, formatResponseTime(duration)),
+		colorize(remoteAddrColor, strings.SplitN(req.RemoteAddr, ":", 2)[0]),
+	)
 }
 
 func (r *MuxRouter) StartServer(srv *http.Server) {
 	gloader.NewWatcher(srv, time.Second, r.logger).Start()
+}
+
+// getColorForStatus returns the color for the given HTTP status code
+func getColorForStatus(status int) string {
+	switch {
+	case status >= 200 && status < 300:
+		return ColorGreen // Success status code
+	case status >= 400 && status < 500:
+		return ColorYellow // Client error status code
+	case status >= 500:
+		return ColorRed // Server error status code
+	default:
+		return ColorReset // Other status codes
+	}
+}
+
+// colorize adds color to a string
+func colorize(color, text string) string {
+	return fmt.Sprintf("%s%s%s", color, text, ColorReset)
 }
